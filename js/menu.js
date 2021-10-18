7dcd5ec583f8e816e9541cc056efc4a45ba5f7b4
Annylys Medeiros <annylysmedeiros@hotmail.com>
2021-10-18 20:45:10 -0300

    ajuste na responsividade do site, inclusão de informações e ajuste de cores.

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-const menu_mobile = document.querySelector('.menu_mobile');
-   const navLinks = document.querySelector('.links_menu2');  
-   menu_mobile.addEventListener('click', () =>{
-    navLinks.classList.toggle('open');
-   });
