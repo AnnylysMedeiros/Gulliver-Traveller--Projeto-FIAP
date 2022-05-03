0558a6ad3f7d6bd205616f34befcab0326c8af70
Annylys Medeiros <annylysmedeiros@hotmail.com>
2022-05-02 22:10:20 -0300

    Alteração no mapa de geolocalização.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const encuentrame = () => {
-    //todo o processo de localização
+   // todo o processo de localização
 
     const success = (position) => {
         const divCoordenadas = document.getElementById("coordenadas");
@@ -16,7 +16,7 @@ const encuentrame = () => {
         mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=jT0QRLARruiIO5jNjuNQZxb4G7AAmi5b&locations=${latitude},${longitude}&size=700,300`;
         divMapa.appendChild(mapa);
     };
-    // AjDlDSYRo393ah_QeRYxKK7BG0dcbCkyuQJ07GIi49QEjzSaQszaqga--ysNp1df
+  //  AjDlDSYRo393ah_QeRYxKK7BG0dcbCkyuQJ07GIi49QEjzSaQszaqga--ysNp1df
 
     const errorPosicion = (error) => {
         const divCoordenadas = document.getElementById("coordenadas");
@@ -28,7 +28,7 @@ const encuentrame = () => {
 
 const verificarGeo = () => {
     if(!navigator.geolocation){
-    //não suporta localização
+  //  não suporta localização
     const divCoordenadas = document.getElementById("coordenadas");
     divCoordenadas.innerHTML ="<p>localização não suportada pelo navegador</>";
     return;
@@ -38,6 +38,54 @@ const verificarGeo = () => {
 };
 
 verificarGeo();
+
+
+
+
+//api google
+// let map;
+// let service;
+// let infowindow;
+
+// function initMap() {
+//     const saoPaulo = new google.maps.LatLng(-23.4157393, -46.5475598);
+
+//     infowindow = new google.maps.InfoWindow();
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         center: saoPaulo,
+//         zoom: 15,
+//     });
+
+//     const request = {
+//         query: "Museum of Contemporary Art Australia",
+//         fields: ["name", "geometry"],
+//     };
+
+//     service = new google.maps.places.PlacesService(map);
+//     service.findPlaceFromQuery(request, (results, status) => {
+//         if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+//             for (let i = 0; i < results.length; i++) {
+//                 createMarker(results[i]);
+//             }
+
+//             map.setCenter(results[0].geometry.location);
+//         }
+//     });
+// }
+
+// function createMarker(place) {
+//     if (!place.geometry || !place.geometry.location) return;
+
+//     const marker = new google.maps.Marker({
+//         map,
+//         position: place.geometry.location,
+//     });
+
+//     google.maps.event.addListener(marker, "click", () => {
+//         infowindow.setContent(place.name || "");
+//         infowindow.open(map);
+//     });
+// }
 
 
 
