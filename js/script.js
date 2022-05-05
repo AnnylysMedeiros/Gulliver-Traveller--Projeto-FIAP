a8c129b17ff827bbd92759e4b0cfecb601caa932
Annylys Medeiros <annylysmedeiros@hotmail.com>
2022-05-05 17:28:17 -0300

    Inclusão de API de geolocalização com google maps

--- js/script.js
+++ js/script.js
@@ -1,44 +1,96 @@
-const encuentrame = () => {
-   // todo o processo de localização
+// const encuentrame = () => {
+//    // todo o processo de localização
 
-    const success = (position) => {
-        const divCoordenadas = document.getElementById("coordenadas");
-        const divMapa = document.getElementById("mapa");
-        console.log(position);
-        const { latitude , longitude } = position.coords;
-        console.log(latitude, longitude);
+//     const success = (position) => {
+//         const divCoordenadas = document.getElementById("coordenadas");
+//         const divMapa = document.getElementById("mapa");
+//         console.log(position);
+//         const { latitude , longitude } = position.coords;
+//         console.log(latitude, longitude);
 
-        divCoordenadas.innerHTML = `lat: ${latitude}, lng: ${longitude}`;
+//         divCoordenadas.innerHTML = `lat: ${latitude}, lng: ${longitude}`;
     
-        //mapa
-        // https://www.mapquestapi.com/staticmap/v5/map?key=KEY&locations=LOCATION&size=WIDTH,HEIGHT
-        const mapa = new Image();
-        mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=jT0QRLARruiIO5jNjuNQZxb4G7AAmi5b&locations=${latitude},${longitude}&size=700,300`;
-        divMapa.appendChild(mapa);
-    };
-  //  AjDlDSYRo393ah_QeRYxKK7BG0dcbCkyuQJ07GIi49QEjzSaQszaqga--ysNp1df
+//         //mapa
+//         // https://www.mapquestapi.com/staticmap/v5/map?key=KEY&locations=LOCATION&size=WIDTH,HEIGHT
+//         const mapa = new Image();
+//         mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=jT0QRLARruiIO5jNjuNQZxb4G7AAmi5b&locations=${latitude},${longitude}&size=700,300`;
+//         divMapa.appendChild(mapa);
+//     };
+//   //  AjDlDSYRo393ah_QeRYxKK7BG0dcbCkyuQJ07GIi49QEjzSaQszaqga--ysNp1df
 
-    const errorPosicion = (error) => {
-        const divCoordenadas = document.getElementById("coordenadas");
-        divCoordenadas.innerHTML = `Erro ao obter localização:<br> Error ${error.code}: ${error.message}`;
-    };
+//     const errorPosicion = (error) => {
+//         const divCoordenadas = document.getElementById("coordenadas");
+//         divCoordenadas.innerHTML = `Erro ao obter localização:<br> Error ${error.code}: ${error.message}`;
+//     };
 
-    navigator.geolocation.getCurrentPosition( success , errorPosicion );
-};
+//     navigator.geolocation.getCurrentPosition( success , errorPosicion );
+// };
 
-const verificarGeo = () => {
-    if(!navigator.geolocation){
-  //  não suporta localização
-    const divCoordenadas = document.getElementById("coordenadas");
-    divCoordenadas.innerHTML ="<p>localização não suportada pelo navegador</>";
-    return;
-    }
- //Se suportar, chamar a localização
-  encuentrame();
-};
+// const verificarGeo = () => {
+//     if(!navigator.geolocation){
+//   //  não suporta localização
+//     const divCoordenadas = document.getElementById("coordenadas");
+//     divCoordenadas.innerHTML ="<p>localização não suportada pelo navegador</>";
+//     return;
+//     }
+//  //Se suportar, chamar a localização
+//   encuentrame();
+// };
 
-verificarGeo();
+// verificarGeo();
 
+
+///////////////////////
+
+
+
+// var x=document.getElementById("demo");
+        function getLocation()
+          {
+          if (navigator.geolocation)
+            {
+            navigator.geolocation.getCurrentPosition(showPosition,showError);
+            }
+          else{x.innerHTML="Geolocalização não é suportada nesse browser.";}
+          }
+         
+        function showPosition(position)
+          {
+          lat=position.coords.latitude;
+          lon=position.coords.longitude;
+          latlon=new google.maps.LatLng(lat, lon)
+          mapholder=document.getElementById('mapholder')
+          mapholder.style.height='20rem';
+          mapholder.style.width='40rem';
+         
+          var myOptions={
+          center:latlon,zoom:14,
+          mapTypeId:google.maps.MapTypeId.ROADMAP,
+          mapTypeControl:false,
+          navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+          };
+          var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+          var marker=new google.maps.Marker({position:latlon,map:map,title:"Você está Aqui!"});
+          }
+         
+        function showError(error)
+          {
+          switch(error.code)
+            {
+            case error.PERMISSION_DENIED:
+              x.innerHTML="Usuário rejeitou a solicitação de Geolocalização."
+              break;
+            case error.POSITION_UNAVAILABLE:
+              x.innerHTML="Localização indisponível."
+              break;
+            case error.TIMEOUT:
+              x.innerHTML="O tempo da requisição expirou."
+              break;
+            case error.UNKNOWN_ERROR:
+              x.innerHTML="Algum erro desconhecido aconteceu."
+              break;
+            }
+          }
 
 
 
